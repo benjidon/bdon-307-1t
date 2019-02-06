d4cef370110bbb909dc09087c0779aca05abec8d
Benjamin Don <zallzool@Bens-MacBook-Pro.local>
2019-02-06 10:52:43 -0800

    First

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	
+	public int multiply(int i, int k) {
+		
+		return i * k;
+	}
+	
+	public int main(String[] args)  {
+		
+		System.out.println("hi");
+		multiply(1,2);
+		return 0;
+		
+	}
+}
